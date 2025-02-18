c97d94dd28df762a9598b1d512e0025338380e6b
Laércio Crestani <lrcrestani@gmail.com>
2025-02-17 22:06:19 -0300

    home lang

--- resources/lang/it/home.php
+++ resources/lang/it/home.php
@@ -7,7 +7,68 @@ return [
     ],
     'concorsi' => [
         'h2' => 'Concorsi pubblici',
-        'p' => 'Selexi è il partner ideale per la gestione di concorsi pubblici. Grazie a un approccio integrato e a soluzioni innovative, supportiamo la Pubblica Amministrazione in ogni fase delle procedure concorsuali. Con Selexi, ogni progetto viene gestito con competenza, affidabilità e personalizzazione sulla base delle specifiche esigenze dei nostri clienti.',
-        'cta' => 'Scopri di più'
-    ]
+        'btn' => "Leggi I’articolo",
+        'btn_b' => "Guarda tutti",
+    ],
+    'servizi' => [
+        'p' => 'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.',
+        'title_a' => "Assessment",
+        'p_a' => "Supportiamo le amministrazioni pubbliche nella gestione delle procedure concorsuali attraverso processi di Assessment innovativi e mirati.",
+        'title_b' => 'Examonline',
+        'p_b' => "Con ExamOnline, la nostra piattaforma digitale per la gestione e la somministrazione delle prove d'esame per concorsi, garantiamo un processo sicuro, trasparente ed efficace. Semplifica l’organizzazione, riduci i tempi e offri un'esperienza intuitiva ai candidati, dalla registrazione al risultato finale.",
+        'title_c' => "Test Center",
+        'p_c' => "La nostra esperienza nella gestione test center assicura un ambiente ottimale per la somministrazione delle prove d'esame per concorsi.",
+        'btn' => '...'
+    ],
+    'virtuale' => [
+        'title' => "Realtà virtuale",
+        'p' => "Grazie alla piattaforma per la gestione concorsi pubblici, offriamo esperienze immersive per i candidati, migliorando l'organizzazione delle prove d'esame e la trasparenza delle procedure."
+    ],
+    'rivolgiamo' => [
+        'title' => "A chi ci rivolgiamo",
+        'p' => "Ci rivolgiamo a enti pubblici, aziende e istituzioni che cercano una gestione dei concorsi pubblici moderna ed efficiente. Grazie a strumenti tecnologicamente avanzati, come il nostro software dedicato alla gestione telematica dei concorsi, offriamo soluzioni affidabili e innovative per ottimizzare ogni fase del processo, dalla pianificazione alla somministrazione delle prove.",
+        'check_a' => "Scoprire i tuoi punti di forza e diventare competitivo nel contesto globale.",
+        'check_b' => "Scoprire i tuoi punti di forza e diventare competitivo nel contesto globale.",
+        'check_c' => "Scoprire i tuoi punti di forza e diventare competitivo nel contesto globale.",
+    ],
+    'professionisti' => [
+        'title' => "I nostri professionisti",
+        'p' => "Il nostro team di esperti è altamente qualificato nella selezione e valutazione dei test per enti pubblici e aziende. Offriamo soluzioni personalizzate per rispondere a ogni esigenza concorsuale, garantendo professionalità, precisione e un approccio su misura per ogni progetto."
+    ],
+    'scegliere' => [
+        'title' => "Perchè scegliere Selexi",
+        'p' => "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo sed quia consequuntur magni dolores eos. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo sed quia consequuntur magni dolores eos.",
+        'title_a' => "Innovazione",
+        'info_a' => "Utilizziamo il miglior software di gestione concorsi pubblici per garantire trasparenza e affidabilità.",
+        'title_b' => "Efficienza",
+        'info_b' => "La nostra piattaforma di gestione concorsi pubblici accelera i tempi e riduce i costi.",
+        'title_c' => "Esperienza",
+        'info_c' => "Oltre 20 anni di esperienza nell'organizzazione di prove d'esame e gestione concorsi informatizzati e telematici.",
+        'title_d' => "Supporto completo",
+        'info_d' => "Dalla somministrazione dei test per concorsi alla valutazione dei risultati, ci occupiamo di ogni fase.",
+        'btn' => '...'
+    ],
+    'dicono' => [
+        'title' => "Cosa Dicono di noi",
+        'p' => "Selexi è riconosciuta per la sua capacità di innovare e offrire soluzioni all’avanguardia nella gestione delle procedure concorsuali. La soddisfazione dei nostri clienti è il nostro miglior biglietto da visita: scopri le loro recensioni e lasciati guidare dalle esperienze positive di chi ha scelto i nostri servizi."
+    ],
+    'news' => [
+        'title' => "Le ultime news"
+    ],
+    'informazioni' => [
+        'title' => "Richiedi informazioni",
+        'p' => "Per maggiori dettagli sui nostri servizi e soluzioni, compila il modulo e sarai ricontattato al più presto.",
+        'name' => 'Name',
+        'last_name' => 'Last Name',
+        'phone' => 'Phone',
+        'email' => 'Email',
+        'message' => 'Message',
+        'check_a' => '...',
+        'check_b' => '...',
+        'check_c' => '...',
+        'btn' => 'Invia  richiesta'
+    ],
+    'certificazioni' => [
+        'title' => "Certificazioni"
+    ],
 ];
